f9322e4dfdb9ea69e99fb44f2201f27baee57209
dvakota <czpt101@gmail.com>
2015-07-04 10:33:03 -0700

    Added new challenges

---
+++ training/codeeval/CodeEvalLongestSubstring.java
@@ -0,0 +1,44 @@
+package gk.training.codeeval;
+
+public class CodeEvalLongestSubstring {
+
+    public static void main(String[] args) {
+        String s = "thisisatest;testing123testing";
+
+
+        String[] arr = s.split(";");
+        System.out.println(lcs(arr[0], arr[1]));
+    }
+
+    public static String lcs(String s1, String s2) {
+        int[][] table = new int[s1.length()+1][s2.length()+1];
+
+        for (int i = 0; i < s1.length(); i++)
+            for (int j = 0; j < s2.length(); j++)
+                if (s1.charAt(i) == s2.charAt(j)) {
+                    table[i+1][j+1] = table[i][j] + 1;
+                } else {
+                    table[i+1][j+1] = Math.max(table[i + 1][j], table[i][j+1]);
+                }
+
+        String result = "";
+        for (int row = s1.length(), col = s2.length(); row != 0 && col != 0; ) {
+            if (table[row][col] == table[row-1][col]) {
+                row--;
+            } else {
+                if (table[row][col] == table[row][col-1]) {
+                    col--;
+                } else {
+                    result = s1.charAt(row-1) + result;
+                    row--; col--;
+                }
+            }
+        }
+        return result;
+    }
+
+
+    public static void say(Object o) {
+        System.out.println(o);
+    }
+}
---
+++ training/codeeval/CodeEvalParehtns.java
@@ -0,0 +1,41 @@
+package gk.training.codeeval;
+
+import java.util.Stack;
+
+/**
+ * Date: 7/2/15
+ */
+public class CodeEvalParehtns {
+    public static void main(String[] args) {
+        String[] tests = {"", "{[]}", "({}[()})", "[{}[]", "{([]aa kh})", "{[aaa(hkh)]}", "((((}))))))"};
+        for (String test : tests) {
+            System.out.println(isValid(test) ? "True" : "False");
+        }
+        System.out.println(Integer.toBinaryString(15 & 10));
+    }
+
+    public static boolean isValid(String str) {
+
+        Stack<Character> stack = new Stack<Character>();
+        for (char c : str.toCharArray()) {
+           if ("{[(".contains(c + "")) stack.push(c);
+           else {
+               if (!"]})".contains(c + "")) continue;
+               if (stack.isEmpty()) return false;
+               char popped = stack.pop();
+               if (popped != pairof(c)) return false;
+           }
+        }
+        if (!stack.isEmpty()) return false;
+        return true;
+    }
+
+    public static char pairof(char c) {
+        switch (c) {
+            case ']': return '[';
+            case '}': return '{';
+            case ')': return '(';
+        }
+        return ' ';
+    }
+}
